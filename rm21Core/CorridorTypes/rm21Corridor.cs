c9e43f3153df9c28bf6c22487b631b0f84258800
Paul Schrum <paul.schrum@gmail.com>
2012-09-14 21:36:29 -0400

    Added new WPF Project: MainRM21WPFapp.  This is my first attempt at actually writing an application. Its first intended use is as a demo while I can not develop under Microstation.  But if I have done my homeork well, this could eventually become the application for running under WPF-only.

---
+++ rm21Core/CorridorTypes/rm21Corridor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ptsCogo.coordinates.CurvilinearCoordinates;
+using ptsCogo;
+using ptsCogo.Angle;
+
+namespace rm21Core
+{
+   /// <summary>
+   /// This class represents all surfaces controlled by the same chain, 
+   ///   including medians.  Eventually this class will be made abstract
+   ///   and roadways will be implements by a subclass, just as riparian
+   ///   ways and railways will be.
+   /// </summary>
+   public class rm21Corridor
+   {
+      private List<PGLGrouping> allPGLgroupings_;
+
+      public void addPGLgrouping(PGLGrouping aPGLgrouping)
+      {
+         if (aPGLgrouping == null)
+            throw new ArgumentNullException();
+
+         if (allPGLgroupings_ == null)
+            allPGLgroupings_ = new List<PGLGrouping>();
+
+         allPGLgroupings_.Add(aPGLgrouping);
+      }
+
+      public bool getElevation(ref StationOffsetElevation soePoint)
+      {
+         foreach (var pglGr in allPGLgroupings_)
+         {
+            int isOnThisPGLgrp = pglGr.getElevationFromSOE(ref soePoint);
+            if (isOnThisPGLgrp == 0)
+               return true;
+         }
+
+         return false;
+      }
+
+   }
+
+}
+
+// I think I need
+// class negatableIndexList<T> : List<T>
+// which would cover my worries about iterating from -2 to +2
+// while skipping zero.
+
+
