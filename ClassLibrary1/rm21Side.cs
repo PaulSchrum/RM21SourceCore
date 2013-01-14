63b5fe4b560661ffcea61be5d786eea065045490
Paul Schrum <paul.schrum@gmail.com>
2013-01-13 21:43:15 -0500

    Class to put -1 as left and +1 as right into code.

---
+++ ClassLibrary1/rm21Side.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ptsCogo
+{
+   public static class rm21SideHelper
+   {
+      public static void toggle(rm21Side s)
+      {
+         if (s == rm21Side.Left)
+            s = rm21Side.Right;
+         else
+            s = rm21Side.Left;
+      }
+   }
+
+   public enum rm21Side
+   {
+      Left = -1,
+      Right = 1
+   }
+}
