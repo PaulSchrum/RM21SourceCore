129cd79ca0653703ca187397a03060338fd8cdf8
Paul Schrum <paul.schrum@gmail.com>
2013-01-14 21:05:00 -0500

    Enhanced features:  Added Inside/Outside; Added a class to combine the two so it can be used as a multiplier; Added ToString() override.

--- ClassLibrary1/rm21Side.cs
+++ ClassLibrary1/rm21Side.cs
@@ -5,20 +5,42 @@ using System.Text;
 
 namespace ptsCogo
 {
-   public static class rm21SideHelper
+   public class rm21Side
    {
-      public static void toggle(rm21Side s)
+      public rm21RightLeftSide RLside { get; set; }
+      public rm21InOutSide InOutside { get; set; }
+
+      public int Sign()
       {
-         if (s == rm21Side.Left)
-            s = rm21Side.Right;
-         else
-            s = rm21Side.Left;
+         return (int) RLside * (int) InOutside;
+      }
+
+      public override string ToString()
+      {
+         return InOutside.GetType().ToString() + " " + RLside.GetType().ToString();
       }
    }
 
-   public enum rm21Side
+   public static class rm21SideHelper
+   {
+      public static void toggle(rm21RightLeftSide s)
+      {
+         if (s == rm21RightLeftSide.Left)
+            s = rm21RightLeftSide.Right;
+         else
+            s = rm21RightLeftSide.Left;
+      }
+   }
+
+   public enum rm21RightLeftSide
    {
       Left = -1,
       Right = 1
+   }
+
+   public enum rm21InOutSide
+   {
+      Inside = -1,
+      Outside = 1
    }
 }
