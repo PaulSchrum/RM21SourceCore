d9e2e51e19b79ecd20602ca5d3b77d21c6a71eaa
Paul Schrum <paul.schrum@gmail.com>
2013-04-13 15:02:10 -0400

    Added Open Channel class.

---
+++ rm21Core/CorridorTypes/rm21OpenChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rm21Core.CorridorTypes
+{
+   class rm21OpenChannel : rm21Corridor
+   {
+      public rm21OpenChannel(string name) : base(name) { }
+   }
+}
