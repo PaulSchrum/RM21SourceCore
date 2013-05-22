372cf3d6b31b0b814c669e4db590bdf4b842df04
Paul Schrum <paul.schrum@gmail.com>
2013-05-21 20:06:13 -0400

    Added Channel Bottom Ribbon

---
+++ rm21Core/Ribbons/ChannelBottom.cs
@@ -0,0 +1,20 @@
+﻿using ptsCogo;
+using ptsCogo.Angle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rm21Core.Ribbons
+{
+   class ChannelBottom : ribbonBase
+   {
+      public ChannelBottom(CogoStation beginStation, CogoStation endStation, double initialWidth, Slope initialSlope)
+         : base(beginStation, endStation, initialWidth, initialSlope) { }
+
+      public ChannelBottom(Double width, Slope slope) : base(width, slope) { }
+
+      public override string getHashName() { return "Channel Bottom"; }
+   
+   }
+}
