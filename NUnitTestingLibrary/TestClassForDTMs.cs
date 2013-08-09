2b39e0eb75c07fae94376c00f7ed2e1c550deca6
Paul Schrum <paul.schrum@gmail.com>
2013-08-09 18:03:12 -0400

    Added test for TIN class.
    TIN is Triangulated Irregular Network.

--- NUnitTestingLibrary/TestClassForDTMs.cs
+++ NUnitTestingLibrary/TestClassForDTMs.cs
@@ -36,10 +36,67 @@ namespace NUnitTestingLibrary
       }
 
       [Test]
-      public void FakeTest()
+      public void TIN_whenPointIsOutsideBoundingBox_shouldReturnNull()
       {
-         int i = 0;
-         Assert.True(true);
+         double? elev;
+         ptsPoint testPt = new ptsPoint(2082100.0, 74200.0, 0.0);
+         elev = aDTM.getElevation(testPt);
+
+         Assert.IsNull(elev);
+      }
+
+      [Test]
+      public void TIN_whenPointIsInsideBoundingBoxAndOutsideTinHull_shouldReturnNull()
+      {
+         double? elev;
+         ptsPoint testPt = new ptsPoint(2083000, 740648.0, 0.0);
+         elev = aDTM.getElevation(testPt);
+
+         Assert.IsNull(elev);
+      }
+
+      //[Test]
+      //public void TIN_whenXYpointIsInsdeInternalTriangle_ElevationIsCorrect()
+      //{
+      //   double expectedDbl, actualDbl;
+      //   expectedDbl = 464.223;
+      //   ptsPoint testPt = new ptsPoint(2083057.836, 740822.223, 0.0);
+      //   actualDbl = (Double)aDTM.getElevation(testPt);
+
+      //   Assert.AreEqual(expected: expectedDbl, actual: actualDbl, delta: 0.0015);
+      //}
+
+      [Test]
+      public void TIN_whenXYpointIsOnTheTINhull_ElevationIsCorrect()
+      {
+         double expectedDbl, actualDbl;
+         expectedDbl = 454.504;
+         ptsPoint testPt = new ptsPoint(2082985.4480, 740657.1722, 0.0);
+         actualDbl = (Double)aDTM.getElevation(testPt);
+
+         Assert.AreEqual(expected: expectedDbl, actual: actualDbl, delta: 0.0015);
+      }
+
+      [Test]
+      public void TIN_whenXYpointIsOnInternalTriangleLine_ElevationIsCorrect()
+      {
+         double expectedDbl, actualDbl;
+         expectedDbl = 464.0131;
+         ptsPoint testPt = new ptsPoint(2083058.8956, 740819.1854, 0.0);
+         actualDbl = (Double)aDTM.getElevation(testPt);
+
+         Assert.AreEqual(expected: expectedDbl, actual: actualDbl, delta: 0.0015);
+      }
+
+      [Test]
+      public void TIN_whenXYpointIsOnInternalTriangleVertex_ElevationIsCorrect()
+      {
+         double expectedDbl, actualDbl;
+         expectedDbl = 482.5578;
+         ptsPoint testPt = new ptsPoint(2082886.0883, 740821.373, 0.0);
+         actualDbl = (Double)aDTM.getElevation(testPt);
+
+         Assert.AreEqual(expected: expectedDbl, actual: actualDbl, delta: 0.0015);
       }
 
    }
