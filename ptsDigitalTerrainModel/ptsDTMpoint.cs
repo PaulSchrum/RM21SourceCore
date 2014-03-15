80d616ab644f1e234fa170f70817fe4dcdad68e8
Paul Schrum <paul.schrum@gmail.com>
2014-03-15 13:33:35 -0400

    Made private var also be static
    Improves performance 13% on the large dataset dataset.

--- ptsDigitalTerrainModel/ptsDTMpoint.cs
+++ ptsDigitalTerrainModel/ptsDTMpoint.cs
@@ -7,15 +7,32 @@ using ptsCogo;
 namespace ptsDigitalTerrainModel
 {
    [Serializable]
+   public struct efficientPoint
+   {
+      public efficientPoint(String Index, String X, String Y, String Z)
+      {
+         index = UInt64.Parse(Index);
+         x = Double.Parse(X);
+         y = Double.Parse(Y);
+         z = Double.Parse(Z);
+      }
+
+      public UInt64 index;
+      public Double x;
+      public Double y;
+      public Double z;
+   }
+
+   [Serializable]
    public class ptsDTMpoint : ptsCogo.ptsPoint
    {
-      public UInt64 myIndex { get; private set; }
+      //public UInt64 myIndex { get; private set; }
 
       [NonSerialized]
-      private String[] parsedStrings;
+      private static String[] parsedStrings;
 
       public ptsDTMpoint(double newX, double newY, double newZ)
-      { x = newX; y = newY; z = newZ; myIndex = 0L; }
+      { x = newX; y = newY; z = newZ; } //myIndex = 0L; }
 
       public ptsDTMpoint(String ptAsString, UInt64 myIndx)
       {
@@ -23,14 +40,14 @@ namespace ptsDigitalTerrainModel
          Double.TryParse(parsedStrings[0], out base.x_);
          Double.TryParse(parsedStrings[1], out base.y_);
          Double.TryParse(parsedStrings[2], out base.z_);
-         myIndex = myIndx;
+         //myIndex = myIndx;
       }
 
       private ptsDTMpoint() { }
 
       public ptsDTMpoint(String x, String y, String z)
          : base(x, y, z)
-      {  myIndex = 0L;   }
+      {  }  //myIndex = 0L;   }
 
       static public ptsDTMpoint getAveragePoint(ptsDTMpoint pt1, ptsDTMpoint pt2, ptsDTMpoint pt3)
       {
