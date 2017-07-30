ad4087693c00fce2e082346381a2c3f5b0e729eb
Paul Schrum <paul.schrum@gmail.com>
2017-07-30 19:12:47 -0400

    Format only: changed tab spacing from 3 to 4 for Azimuth.cs.

--- ptsBoundingBox/Angle/Azimuth.cs
+++ ptsBoundingBox/Angle/Azimuth.cs
@@ -5,139 +5,139 @@ using System.Text;
 
 namespace ptsCogo.Angle
 {
-   sealed public class Azimuth : ptsAngle
-   {
-      public Azimuth() { }
+    sealed public class Azimuth : ptsAngle
+    {
+        public Azimuth() { }
 
-      public Azimuth(double anAngleDbl)
-      {
-         angle_ = anAngleDbl;
-      }
+        public Azimuth(double anAngleDbl)
+        {
+            angle_ = anAngleDbl;
+        }
 
-      public Azimuth(ptsDegree deg)
-      {
-         this.angle__ = Math.Atan2(ptsDegree.Sin(deg), ptsDegree.Cos(deg));
-      }
+        public Azimuth(ptsDegree deg)
+        {
+            this.angle__ = Math.Atan2(ptsDegree.Sin(deg), ptsDegree.Cos(deg));
+        }
 
-      public Azimuth(ptsPoint beginPt, ptsPoint endPt)
-      {
-         this.angle__ = Math.Atan2(endPt.y - beginPt.y, endPt.x - beginPt.x);
-      }
+        public Azimuth(ptsPoint beginPt, ptsPoint endPt)
+        {
+            this.angle__ = Math.Atan2(endPt.y - beginPt.y, endPt.x - beginPt.x);
+        }
 
-      public new double angle_ { get { return getAsAzimuth(); } set { base.normalize(value); } }
+        public new double angle_ { get { return getAsAzimuth(); } set { base.normalize(value); } }
 
-      public Azimuth reverse()
-      {
-         return new Azimuth(this.angle__ + Math.PI);
-      }
-      
-      //public override void setFromXY(double x, double y)
-      //{
-      //   double dbl = Math.Atan2(x, y);
-      //   angle_ = dbl;
-      //}
+        public Azimuth reverse()
+        {
+            return new Azimuth(this.angle__ + Math.PI);
+        }
 
-      private double getAsAzimuth()
-      {
-         double retVal;
+        //public override void setFromXY(double x, double y)
+        //{
+        //   double dbl = Math.Atan2(x, y);
+        //   angle_ = dbl;
+        //}
 
-         retVal = (-1.0 * base.angle_) + (Math.PI / 2.0);
+        private double getAsAzimuth()
+        {
+            double retVal;
 
-         return retVal;
-      }
+            retVal = (-1.0 * base.angle_) + (Math.PI / 2.0);
 
-      public override double getAsDegreesDouble()
-      {
-         double retValueDbl = getAsAzimuth() * 180 / Math.PI;
-         return retValueDbl >= 0.0 ? retValueDbl : retValueDbl + 360.0;
-      }
+            return retVal;
+        }
 
-      public override ptsDegree getAsDegrees()
-      {
-         ptsDegree retValueDeg = getAsAzimuth() * 180 / Math.PI;
-         return retValueDeg >= 0.0 ? retValueDeg : retValueDeg + 360.0;
-      }
+        public override double getAsDegreesDouble()
+        {
+            double retValueDbl = getAsAzimuth() * 180 / Math.PI;
+            return retValueDbl >= 0.0 ? retValueDbl : retValueDbl + 360.0;
+        }
 
-      public override void setFromDegreesDouble(double degrees)
-      {
-         //double adjustedDegrees = ((degrees / -180.0)+ 1) *180.0;
-         double radians = degrees * Math.PI / 180.0;
-         angle_ = Math.Atan2(Math.Cos(radians), Math.Sin(radians));  // This is flipped intentionally
+        public override ptsDegree getAsDegrees()
+        {
+            ptsDegree retValueDeg = getAsAzimuth() * 180 / Math.PI;
+            return retValueDeg >= 0.0 ? retValueDeg : retValueDeg + 360.0;
+        }
 
-      }
+        public override void setFromDegreesDouble(double degrees)
+        {
+            //double adjustedDegrees = ((degrees / -180.0)+ 1) *180.0;
+            double radians = degrees * Math.PI / 180.0;
+            angle_ = Math.Atan2(Math.Cos(radians), Math.Sin(radians));  // This is flipped intentionally
 
-      public override void setFromDegreesMinutesSeconds(int degrees, int minutes, double seconds)
-      {
-         setFromDegreesDouble(
-               (double)degrees + (double)minutes / 60.0 + seconds / 3600.0
-                        );
-      }
+        }
 
-      public static int getQuadrant(double angleDegrees)
-      {
-         return (int)Math.Round((angleDegrees / 90.0) + 0.5);
-      }
+        public override void setFromDegreesMinutesSeconds(int degrees, int minutes, double seconds)
+        {
+            setFromDegreesDouble(
+                  (double)degrees + (double)minutes / 60.0 + seconds / 3600.0
+                           );
+        }
 
-      //to do:
-      //setAsAzimuth
-      //getAsDegreeMinuteSecond
-      //setAsDegree
-      //setAsDegreeMinuteSecond
-      //yada
+        public static int getQuadrant(double angleDegrees)
+        {
+            return (int)Math.Round((angleDegrees / 90.0) + 0.5);
+        }
 
-      public static Azimuth newAzimuthFromAngle(ptsAngle angle)
-      {
-         Azimuth retAz = new Azimuth();
-         retAz.setFromDegreesDouble(angle.getAsDegreesDouble());
-         return retAz;
-      }
+        //to do:
+        //setAsAzimuth
+        //getAsDegreeMinuteSecond
+        //setAsDegree
+        //setAsDegreeMinuteSecond
+        //yada
 
-      // operator overloads
-      public static implicit operator Azimuth(double angleAs_double)
-      {
-         Azimuth anAzimuth = new Azimuth();
-         anAzimuth.angle_ = angleAs_double;
-         return anAzimuth;
-      }
+        public static Azimuth newAzimuthFromAngle(ptsAngle angle)
+        {
+            Azimuth retAz = new Azimuth();
+            retAz.setFromDegreesDouble(angle.getAsDegreesDouble());
+            return retAz;
+        }
 
-      public static Azimuth operator +(Azimuth anAz, ptsAngle anAngle)
-      {
-         return new Azimuth(anAz.getAsRadians() - anAngle.getAsRadians());  // Note: Subtraction is intentional since azimuths are clockwise
-      }
+        // operator overloads
+        public static implicit operator Azimuth(double angleAs_double)
+        {
+            Azimuth anAzimuth = new Azimuth();
+            anAzimuth.angle_ = angleAs_double;
+            return anAzimuth;
+        }
 
-      public static double operator -(Azimuth Az1, Azimuth Az2)
-      {
-         Double returnDeflection = (Az1.angle_ - Az2.angle_);
-         return ptsAngle.normalizeToPlusOrMinus2PiStatic(returnDeflection);
-      }
+        public static Azimuth operator +(Azimuth anAz, ptsAngle anAngle)
+        {
+            return new Azimuth(anAz.getAsRadians() - anAngle.getAsRadians());  // Note: Subtraction is intentional since azimuths are clockwise
+        }
 
-      public static Azimuth operator +(Azimuth Az1, Deflection defl)
-      {
-         var newAzDeg = Az1.getAsDegreesDouble() + defl.getAsDegreesDouble();
-         Double retDbl = ptsAngle.normalizeToPlusOrMinus360Static(newAzDeg);
-         Azimuth retAz = new Azimuth();
-         retAz.setFromDegreesDouble(retDbl);
-         return retAz;
-      }
+        public static double operator -(Azimuth Az1, Azimuth Az2)
+        {
+            Double returnDeflection = (Az1.angle_ - Az2.angle_);
+            return ptsAngle.normalizeToPlusOrMinus2PiStatic(returnDeflection);
+        }
 
-      public Deflection minus(Azimuth Az2)
-      {
-         Double returnDeflection = (this.angle_ - Az2.angle_);
-         return new Deflection(ptsAngle.normalizeToPlusOrMinus2PiStatic(returnDeflection));
-      }
+        public static Azimuth operator +(Azimuth Az1, Deflection defl)
+        {
+            var newAzDeg = Az1.getAsDegreesDouble() + defl.getAsDegreesDouble();
+            Double retDbl = ptsAngle.normalizeToPlusOrMinus360Static(newAzDeg);
+            Azimuth retAz = new Azimuth();
+            retAz.setFromDegreesDouble(retDbl);
+            return retAz;
+        }
 
-      public override String ToString()
-      {
-         return String.Format("{0:0.0000}°", this.getAsDegreesDouble());
-      }
-   }
+        public Deflection minus(Azimuth Az2)
+        {
+            Double returnDeflection = (this.angle_ - Az2.angle_);
+            return new Deflection(ptsAngle.normalizeToPlusOrMinus2PiStatic(returnDeflection));
+        }
 
-   public static class extendDoubleForAzimuth
-   {
-      public static Azimuth AsAzimuth(this Double val)
-      {
-         return new Azimuth(val);
-      }
-   }
+        public override String ToString()
+        {
+            return String.Format("{0:0.0000}°", this.getAsDegreesDouble());
+        }
+    }
+
+    public static class extendDoubleForAzimuth
+    {
+        public static Azimuth AsAzimuth(this Double val)
+        {
+            return new Azimuth(val);
+        }
+    }
 
 }
