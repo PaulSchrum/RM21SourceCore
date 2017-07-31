a02f9570f244796b816cda0515bfed422263bc7f
Paul Schrum <paul.schrum@gmail.com>
2017-07-31 14:58:50 -0400

    Minor: Corrected indentation to 1 tab = 4 spaces.

--- MainRM21WPFapp/ViewModels/NotifyPropertyChangedVMbase.cs
+++ MainRM21WPFapp/ViewModels/NotifyPropertyChangedVMbase.cs
@@ -7,17 +7,17 @@ using System.Text;
 
 namespace MainRM21WPFapp.ViewModels
 {
-   public class NotifyPropertyChangedVMbase : INotifyPropertyChanged
-   {
-      public event PropertyChangedEventHandler PropertyChanged;
+    public class NotifyPropertyChangedVMbase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
 
-      protected void RaisePropertyChanged(String str) { OnPropertyChanged(str); }
+        protected void RaisePropertyChanged(String str) { OnPropertyChanged(str); }
 
-      protected virtual void OnPropertyChanged(string propertyName)
-      {
-         if (this.PropertyChanged != null)
-            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-      }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if(this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
 
-   }
+    }
 }
