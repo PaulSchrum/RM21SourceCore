09521f34f00b6c787151b7e022001382e1068961
Paul Schrum <paul.schrum@gmail.com>
2017-08-17 17:26:02 -0400

    Formatting: Corrected indentation spaces to 4 per tab.

--- ptsBoundingBox/coordinates/CurvilinearCoordinates/StationOffsetElevation.cs
+++ ptsBoundingBox/coordinates/CurvilinearCoordinates/StationOffsetElevation.cs
@@ -7,30 +7,30 @@ using ptsCogo;
 
 namespace ptsCogo.coordinates.CurvilinearCoordinates
 {
-   public class StationOffsetElevation
-   {
-      //private StationOffsetElevation soePoint;
+    public class StationOffsetElevation
+    {
+        //private StationOffsetElevation soePoint;
 
-      public StationOffsetElevation() { offset = new Offset(0.0); elevation = new Elevation(0.0); }
-      public StationOffsetElevation(double aStation, Offset anOffset, Elevation anElevation)
-      {
-         station = aStation; offset = anOffset; elevation = anElevation;
-      }
+        public StationOffsetElevation() { offset = new Offset(0.0); elevation = new Elevation(0.0); }
+        public StationOffsetElevation(double aStation, Offset anOffset, Elevation anElevation)
+        {
+            station = aStation; offset = anOffset; elevation = anElevation;
+        }
 
-      public StationOffsetElevation(StationOffsetElevation soeOther)
-      {
-         station = soeOther.station;
-         offset = soeOther.offset;
-         elevation = soeOther.elevation;
-      }
+        public StationOffsetElevation(StationOffsetElevation soeOther)
+        {
+            station = soeOther.station;
+            offset = soeOther.offset;
+            elevation = soeOther.elevation;
+        }
 
-      public double station{get; set;}
-      public Offset offset { get; set; }
-      public Elevation elevation { get; set; }
+        public double station { get; set; }
+        public Offset offset { get; set; }
+        public Elevation elevation { get; set; }
 
-      public override string ToString()
-      {
-         return station.ToString() + " " + offset.ToString() + "  (EL: " + elevation.ToString() + ")";
-      }
-   }
+        public override string ToString()
+        {
+            return station.ToString() + " " + offset.ToString() + "  (EL: " + elevation.ToString() + ")";
+        }
+    }
 }
